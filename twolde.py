80d55aa95c302606ab0092f9298088208a3e6ff2
Michael Coffey <coffey@swiftkey.net>
2015-03-25 16:47:22 +0000

    do that python __main__ thing

--- twolde.py
+++ twolde.py
@@ -79,79 +79,79 @@ def do_retweet(id):
 def do_tweet(text, in_reply_to_user_id):
 	olde_api.update_status(status=text, in_reply_to_user_id=in_reply_to_user_id)
 
+if __name__ == '__main__':
+	# very basic argument parsing
+	if len(sys.argv) < 2:
+		usage()
+	elif sys.argv[1] == "install":
+		install()
+	elif sys.argv[1] == "rm":
+		uninstall()
+	elif sys.argv[1] == "run":
+		print '\n\n===Twolde==='
 
-# very basic argument parsing
-if len(sys.argv) < 2:
-	usage()
-elif sys.argv[1] == "install":
-	install()
-elif sys.argv[1] == "rm":
-	uninstall()
-elif sys.argv[1] == "run":
-	print '\n\n===Twolde==='
+		new_auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+		new_auth.set_access_token(sys.argv[3], sys.argv[4])
+		new_api = tweepy.API(new_auth)
 
-	new_auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	new_auth.set_access_token(sys.argv[3], sys.argv[4])
-	new_api = tweepy.API(new_auth)
+		olde_auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+		olde_auth.set_access_token(sys.argv[6], sys.argv[7])
+		olde_api = tweepy.API(olde_auth)
 
-	olde_auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	olde_auth.set_access_token(sys.argv[6], sys.argv[7])
-	olde_api = tweepy.API(olde_auth)
+		s = sched.scheduler(time.time, time.sleep)
 
-	s = sched.scheduler(time.time, time.sleep)
+		now, last_year = get_times()
+		best = now
 
-	now, last_year = get_times()
-	best = now
+		max_id = None
+		user_timeline = tweepy.models.ResultSet()
+		index = 0
 
-	max_id = None
-	user_timeline = tweepy.models.ResultSet()
-	index = 0
+		# iterate back theough pages of 200 tweets until the last tweet is more than a year ago
+		while best > last_year:
+			if max_id:
+				user_timeline += new_api.user_timeline(screen_name=sys.argv[2], count=200, include_rts=1, max_id=(max_id - 1))
+			else:
+				user_timeline += new_api.user_timeline(screen_name=sys.argv[2], count=200, include_rts=1)
 
-	# iterate back theough pages of 200 tweets until the last tweet is more than a year ago
-	while best > last_year:
-		if max_id:
-			user_timeline += new_api.user_timeline(screen_name=sys.argv[2], count=200, include_rts=1, max_id=(max_id - 1))
-		else:
-			user_timeline += new_api.user_timeline(screen_name=sys.argv[2], count=200, include_rts=1)
+			index = len(user_timeline) - 1
 
-		index = len(user_timeline) - 1
+			best = user_timeline[index].created_at
+			max_id = user_timeline[index].id
 
-		best = user_timeline[index].created_at
-		max_id = user_timeline[index].id
-
-		if best < last_year:
-			# go forward through the tweets until we find the first one since a year ago
-			while best < last_year:
-				index -= 1
-				best = user_timeline[index].created_at
+			if best < last_year:
+				# go forward through the tweets until we find the first one since a year ago
+				while best < last_year:
+					index -= 1
+					best = user_timeline[index].created_at
 
 
 
-			while index >= 0:
-				next_tweet = user_timeline[index]
+				while index >= 0:
+					next_tweet = user_timeline[index]
 
-				now, last_year = get_times()
-				sleep_seconds = (next_tweet.created_at - last_year).total_seconds()
+					now, last_year = get_times()
+					sleep_seconds = (next_tweet.created_at - last_year).total_seconds()
 
-				print 'Next tweet: "{}"'.format(next_tweet.text.encode('utf-8'))
-				print 'Next tweet time: {} (in {} seconds)'.format(next_tweet.created_at.ctime(), sleep_seconds)
+					print 'Next tweet: "{}"'.format(next_tweet.text.encode('utf-8'))
+					print 'Next tweet time: {} (in {} seconds)'.format(next_tweet.created_at.ctime(), sleep_seconds)
 
-				# do retweets properly
-				if next_tweet.retweeted:
-					s.enter(max(1, sleep_seconds), 1, do_retweet, [next_tweet.retweeted_status.id])
-				else:
-					s.enter(max(1, sleep_seconds), 1, do_tweet, [next_tweet.text, next_tweet.in_reply_to_user_id])
-				
-				s.run()
+					# do retweets properly
+					if next_tweet.retweeted:
+						s.enter(max(1, sleep_seconds), 1, do_retweet, [next_tweet.retweeted_status.id])
+					else:
+						s.enter(max(1, sleep_seconds), 1, do_tweet, [next_tweet.text, next_tweet.in_reply_to_user_id])
+					
+					s.run()
 
-				index -= 1
+					index -= 1
 
-			# we've run out of tweets since we started so start again
-			user_timeline = tweepy.models.ResultSet()
-			max_id = None
-			best, last_year = get_times()
+				# we've run out of tweets since we started so start again
+				user_timeline = tweepy.models.ResultSet()
+				max_id = None
+				best, last_year = get_times()
 
-	print "I'm finished!"
-else:
-	usage()
+		print "I'm finished!"
+	else:
+		usage()
 
